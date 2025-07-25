e394fba8689d7f551b17ca1be351f380730cd02d
thapThepNgamNual <684259038@webmail.npru.ac.th>
2025-07-25 11:24:17 +0700

    First commit

---
+++ Sum.java
@@ -0,0 +1,8 @@
+public class Sum {
+    public static void main(String args[]) {
+        int num1 = 4;
+        int num2 = 6;
+        int sum = num1 + num2;
+        System.out.println("sum = " + sum);
+    }
+}
